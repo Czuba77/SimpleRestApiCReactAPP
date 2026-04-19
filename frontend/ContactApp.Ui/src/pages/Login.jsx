cbe5a4d26de9ee3914e4fd07906d9fa84d04697b
Antoni Czuba <s201096@student.pg.edu.pl>
2026-04-19 18:19:51 +0200

    login working

--- frontend/ContactApp.Ui/src/pages/Login.jsx
+++ frontend/ContactApp.Ui/src/pages/Login.jsx
@@ -1,3 +1,59 @@
+import { useState } from "react";
+import axiosInstance from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
-    return <h2>log in todo</h2>
+    const [email,setEmail] = useState('');
+    const [passwordHash,setPasswordHash] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setErrorMsg('');
+
+        try {
+            const response = await axiosInstance.post('/auth/login', {
+                email: email,
+                passwordHash: passwordHash
+            });
+            
+            const token = response.data.token;
+            localStorage.setItem('token', token);
+
+            window.location.href = "/";
+        } catch (error){
+            setErrorMsg('Bad credentials.');
+        }
+    };
+
+    return (
+    <div style={{ maxWidth: '300px', padding: '20px', background: '#fff', borderRadius: '5px' }}>
+        <h2>Login panel</h2>
+        {errorMsg && <div style={{ color: 'red', marginBottom: '10px' }}>{errorMsg}</div>}
+        
+        <form onSubmit={handleSubmit}>
+            <div style={{ marginBottom: '10px' }}>
+                <label>E-mail: </label><br />
+                <input 
+                    type="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    required 
+                />
+            </div>
+            <div style={{ marginBottom: '10px' }}>
+                <label>Password: </label><br />
+                <input 
+                    type="password" 
+                    value={passwordHash} 
+                    onChange={(e) => setPasswordHash(e.target.value)} 
+                    required 
+                />
+            </div>
+            <button type="submit" style={{ padding: '8px 15px', marginTop: '10px' }}>Sign in</button>
+        </form>
+    </div>
+    );
 }
